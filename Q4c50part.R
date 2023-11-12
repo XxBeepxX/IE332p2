19a8ac8c7c35847f5fd246005026ac80ea9b1968
XxBeepxX <ryan.ting@gmail.com>
2023-11-12 17:26:56 -0500

    added q4 c50 stuff

---
+++ Q4c50part.R
@@ -0,0 +1,44 @@
+
+# Imports =====================================================================
+install.packages("caret")
+install.packages("ellipse")
+install.packages("C50")
+library(caret)
+library(C50)
+library(ellipse)
+
+# Load Data ===================================================================
+i <- iris
+
+# change to 1s and 0s
+i$Species = as.factor(ifelse(i$Species == "setosa", "1", "0"))
+
+# partition the data
+index <- createDataPartition(i$Species,p = 0.75, list= FALSE)
+train <- i[index,]
+test <- i[-index,]
+
+# Train Models ================================================================
+set.seed(11)
+
+x <- train[,1:4]
+y <- train[,5]
+
+fitControl <- trainControl(method = "repeatedcv",number = 10, repeats = 10, returnResamp="all")
+control <-trainControl(method="cv", number = 10)
+grid <- expand.grid( .winnow = c(TRUE,FALSE), .trials=c(1,5,10,15,20), .model="tree" )
+
+
+# Train the model using train() function from caret
+model<- train(x=x,y=y,tuneGrid=grid,trControl=control,method="C5.0",verbose=FALSE)
+
+# Print the best model
+print(model)
+
+model
+
+# visualize the resample distributions
+dotplot(model)
+
+#xyplot(model,type = c("g", "p", "smooth"))
+plot(model)
