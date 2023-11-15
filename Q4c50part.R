3474807908e86bad60f9365caf9aef2f3bbda935
alclauss <alclauss@purdue.edu>
2023-11-15 08:52:47 -0500

    Update Q4c50part.R

--- Q4c50part.R
+++ Q4c50part.R
@@ -25,12 +25,20 @@ x <- train[,1:4]
 y <- train[,5]
 
 fitControl <- trainControl(method = "repeatedcv",number = 10, repeats = 10, returnResamp="all")
+
+
 control <-trainControl(method="cv", number = 10)
 grid <- expand.grid( .winnow = c(TRUE,FALSE), .trials=c(1,5,10,15,20), .model="tree" )
 
 
 # Train the model using train() function from caret
 model<- train(x=x,y=y,tuneGrid=grid,trControl=control,method="C5.0",verbose=FALSE)
+
+# Use the test data to find model predictions
+predictions <- predict(model, test)
+
+# Confusion Matrix to show the accuracy of the predicted results vs actual results
+table(predictions, test$Species)
 
 # Print the best model
 print(model)
